552dd041c257a894bd5d4d8d69b72aa3423e448e
Yax <yacine@gildia.pl>
2009-05-18 12:00:26 +0200

    forgot map.resource for categories

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ ActionController::Routing::Routes.draw do |map|
   #   map.namespace :admin do |admin|
   #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
   #     admin.resources :products
-  #   end
+  #   end 
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
@@ -38,6 +38,9 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
+  
+  map.resources :categories
+
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
