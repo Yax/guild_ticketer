a2e2ceb1506b52a736c340dd9ed8ec4113c56ca8
Yax <yacine@gildia.pl>
2009-05-24 19:23:33 +0200

    colorful logs messes up script/console :/

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,29 +8,3 @@ class ApplicationController < ActionController::Base
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 end
-
-#Monkeypatching for colorful logs on windows
-module ActiveRecord
-  module ConnectionAdapters
-    class AbstractAdapter
-      protected
-        def format_log_entry(message, dump = nil)
-        if ActiveRecord::Base.colorize_logging
-          if @@row_even
-            @@row_even = false
-            message_color, dump_color = "0;31;1", "0;1"
-          else
-            @@row_even = true
-            message_color, dump_color = "0;32;1", "0;2"
-          end
-      
-          log_entry = "  \e[#{message_color}m#{message}\e[0m   "
-          log_entry << "\e[#{dump_color}m%#{String === dump ? 's' : 'p'}\e[0m" % dump if dump
-          log_entry
-        else
-          "%s  %s" % [message, dump]
-        end
-      end
-    end
-  end
-end
