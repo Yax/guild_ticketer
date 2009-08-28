5a64342dc464a06675c4dd42176d480784258271
Yax <yacine@gildia.pl>
2009-08-29 00:10:35 +0200

    Unfinished functional/tickets_controller_test.rb

--- test/functional/tickets_controller_test.rb
+++ test/functional/tickets_controller_test.rb
@@ -1,13 +1,55 @@
 require 'test_helper'
 
 class TicketsControllerTest < ActionController::TestCase
-  context "on GET to /tickets" do
-    setup do
-      get :index
+  context "TicketsController " do
+
+    context "on GET to /tickets" do
+      setup do
+        get :index
+      end
+      should_respond_with :success
+      should_render_template :index
+      should_not_set_the_flash
+      should_assign_to :tickets
     end
-    should_respond_with :success
-    should_render_template :index
-    should_not_set_the_flash
+
+    context "on POST to /tickets" do
+      setup do
+        post
+      end
+    end
+
+    context "on GET to /tickets/new" do
+      setup do
+        get :new
+      end
+      should_respond_with :success
+      should_render_template :new
+      should_not_set_the_flash
+      should_assign_to :ticket, :categories
+    end
+
+    context "on GET to /tickets/:id/edit" do
+      setup do
+        get :edit, :id => tickets(:zwrot_tomka).to_param
+      end
+      should_respond_with :success
+      should_render_template :edit
+      should_not_set_the_flash
+      should_assign_to :ticket, :categories
+    end
+
+    context "on GET to /tickets/:id" do
+      setup do
+        get :show, :id => tickets(:zwrot_tomka).to_param
+      end
+      should_respond_with :success
+      should_render_template :show
+      should_not_set_the_flash
+      should_assign_to :ticket
+    end
+
   end
+
 
 end
