8120895358cdff4ecc37923e7909ca94ee478ec7
Yax <yacine@gildia.pl>
2009-09-08 15:19:14 +0200

    Added ticket_type to categories views

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,8 @@ class Category < ActiveRecord::Base
   # t.string :ticket_type
 
   has_many :tickets
-  validates_presence_of :name
+  validates_presence_of :name, :ticket_type
   validates_uniqueness_of :name
   validates_length_of :name, :minimum => 3
+  validates_inclusion_of :ticket_type, :in => TICKET_TYPES
 end
