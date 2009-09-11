9bc460ccbbe408c71b9effcc170745453e56ff53
Yax <yacine@gildia.pl>
2009-09-11 18:05:01 +0200

    Should not destroy category if there are any tickets belonging to it.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,9 +2,19 @@ class Category < ActiveRecord::Base
   # t.string :name
   # t.string :ticket_type
 
+  before_destroy :check_tickets
+
   has_many :tickets
   validates_presence_of :name, :ticket_type
   validates_uniqueness_of :name
   validates_length_of :name, :minimum => 3
   validates_inclusion_of :ticket_type, :in => TICKET_TYPES
+
+  def check_tickets
+    unless self.tickets.empty?
+      errors.add_to_base('Cannot destroy while having tickets')
+      false
+    end
+  end
+
 end
--- test/unit/category_test.rb
+++ test/unit/category_test.rb
@@ -6,5 +6,23 @@ class CategoryTest < ActiveSupport::TestCase
   should_validate_presence_of :name
   should_validate_uniqueness_of :name
   should_ensure_length_at_least :name, 3
+  should_not_allow_values_for :ticket_type, 'ticket', 'uazaaa'
+  TICKET_TYPES.each do |type|
+    should_allow_values_for :ticket_type, type
+  end
+  
+  should 'not allow destroying not empty categories' do
+    category = categories(:zwrot)
+    ticket = tickets(:zwrot_tomka)
+    ticket.category_id = nil
+    category.tickets << ticket
+    assert !category.destroy
+  end
+
+  should 'allow destroying empty categories' do
+    category = categories(:zwrot)
+    category.tickets.destroy_all
+    assert category.destroy
+  end
 
 end
